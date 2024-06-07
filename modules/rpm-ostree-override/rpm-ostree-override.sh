f8a350db96ce61e5435e78433a5c86bc0a4aa46c
lorduskordus <lorduskordus@gmail.com>
2024-06-07 21:18:16 +0200

    Minor edits to 'rpm-ostree-override' module

--- modules/rpm-ostree-override/rpm-ostree-override.sh
+++ modules/rpm-ostree-override/rpm-ostree-override.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/env bash
 
-# Tell build process to exit if there are any errors.
 set -euo pipefail
 
-# Get repo to override from.
-get_yaml_array REPO '.repo[]' "$1"
-# Replace '%OS_VERSION%' with the current Fedora version if it's there,
-# replace fedora-'xx' version with the current one,
-# remove 'fedora-xx' from the .repo filename if it's there.
+# Get repository to override from
+REPO=$(echo "$1" | yq -I=0 ".repo")
+# 1. Replace '%OS_VERSION%' with the current Fedora version if it's there
+# 2. Replace fedora-'xx' version with the current one
+# 3. Remove 'fedora-xx' from the .repo filename if it's there
 REPO=$(sed "s/%OS_VERSION%/${OS_VERSION}/; \
             s/fedora-[^/]\+/fedora-${OS_VERSION}/; \
             s/-fedora-[^/]\+.repo/.repo/" \
             <<< "${REPO}")
 
-# Get packages to override replace.
+# Get packages to replace
 get_yaml_array PACKAGES '.replace[]' "$1"
 PACKAGES=$(echo "${PACKAGES[*]}" | tr -d '\n')
 
-# Ensure there's only one repo and more than
-# one package provided.
-if [[ ${#REPO[@]} -gt 1 ]]; then
-    echo "Error: More than one repo provided."
+# Ensure repository and packages are provided
+if [ -z "${REPO}" ]; then
+    echo "Error: No repository was provided."
     exit 1
 elif [ -z "${PACKAGES}" ]; then
     echo "Error: No packages were provided."
@@ -31,6 +29,6 @@ MAINTAINER=$(awk -F'/' '{print $5}' <<< "${REPO}")
 REPO_NAME=$(awk -F'/' '{print $6}' <<< "${REPO}")
 FILE_NAME=$(awk -F'/' '{print $9}' <<< "${REPO}")
 
-wget -P /etc/yum.repos.d/ ${REPO}
+wget -P "/etc/yum.repos.d/" ${REPO}
 rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:${MAINTAINER}:${REPO_NAME} ${PACKAGES}
-rm /etc/yum.repos.d/${FILE_NAME}
+rm "/etc/yum.repos.d/${FILE_NAME}"
