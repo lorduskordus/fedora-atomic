5473e0eb551552aa594599d77539a8b91d626aa9
lorduskordus <lorduskordus@gmail.com>
2025-01-25 14:38:12 +0100

    Rewrite kernel install stuff

--- files/scripts/common/kernel-post-install.sh
+++ files/scripts/common/kernel-post-install.sh
@@ -6,12 +6,10 @@ set -euo pipefail
 
 echo "Kernel post-install script"
 if [[ ! "${IMAGE_NAME}" =~ nvidia ]]; then
-    echo "- NORMAL image, no action needed"
+    echo "- MAIN image, no action needed"
     exit 0
-else
-    echo "- NVIDIA image, installing drivers"
 fi
 
+echo "- NVIDIA image, installing drivers"
 export IMAGE_NAME=$(awk -F'/' '{print $3}' <<< "${BASE_IMAGE}" | cut -d'-' -f1)
-
 curl -s https://raw.githubusercontent.com/ublue-os/hwe/refs/heads/main/nvidia-install.sh | bash
