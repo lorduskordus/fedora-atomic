d2a69190bb8e10d592b1ba6712c6d25323e4364c
lorduskordus <lorduskordus@gmail.com>
2025-01-29 14:51:31 +0100

    [GNOME] Add pano gnome-extension (through files)

---
+++ files/system/gnome/usr/share/gnome-shell/extensions/pano@elhan.io/thirdparty/highlight_js_lib_languages_shell.js
@@ -0,0 +1,41 @@
+function getDefaultExportFromCjs (x) {
+	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
+}
+
+/*
+Language: Shell Session
+Requires: bash.js
+Author: TSUYUSATO Kitsune <make.just.on@gmail.com>
+Category: common
+Audit: 2020
+*/
+
+/** @type LanguageFn */
+function shell(hljs) {
+  return {
+    name: 'Shell Session',
+    aliases: [
+      'console',
+      'shellsession'
+    ],
+    contains: [
+      {
+        className: 'meta.prompt',
+        // We cannot add \s (spaces) in the regular expression otherwise it will be too broad and produce unexpected result.
+        // For instance, in the following example, it would match "echo /path/to/home >" as a prompt:
+        // echo /path/to/home > t.exe
+        begin: /^\s{0,3}[/~\w\d[\]()@-]*[>%$#][ ]?/,
+        starts: {
+          end: /[^\\](?=\s*$)/,
+          subLanguage: 'bash'
+        }
+      }
+    ]
+  };
+}
+
+var shell_1 = shell;
+
+const shell$1 = /*@__PURE__*/getDefaultExportFromCjs(shell_1);
+
+export { shell$1 as default };
