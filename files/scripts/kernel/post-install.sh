eeb1642e392e15907f090267c929183603725a65
lorduskordus <lorduskordus@gmail.com>
2025-04-16 20:51:25 +0200

    Move kernel stuff to base from core & update all main variants to Fedora 42
    - Cosmic Atomic does not have a 41 build, so its coreos variant is disabled

---
+++ files/scripts/kernel/post-install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Kernel post-install script
+
+set -euo pipefail
+
+build-initramfs () {
+    echo "- Building initramfs"
+    export KERNEL_FLAVOR=""
+    curl -s "https://raw.githubusercontent.com/ublue-os/hwe/refs/heads/main/build-initramfs.sh" | bash
+}
+
+install-nvidia-drivers () {
+    echo "- Installing drivers"
+    export IMAGE_NAME=$(awk -F'/' '{print $3}' <<< "${BASE_IMAGE}" | cut -d'-' -f1)
+    curl -s "https://raw.githubusercontent.com/ublue-os/hwe/refs/heads/main/nvidia-install.sh" | bash
+}
+
+echo "Kernel post-install script"
+
+if [[ "${IMAGE_NAME}" =~ nvidia ]]; then
+    echo "Image: NVIDIA"
+    install-nvidia-drivers
+    build-initramfs
+else
+    echo "Image: MAIN"
+    build-initramfs
+fi
