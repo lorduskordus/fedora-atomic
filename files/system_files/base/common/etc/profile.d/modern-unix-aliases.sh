e7455b4e41935843a0cb81c758b30438a069307e
lorduskordus <lorduskordus@gmail.com>
2025-10-29 12:16:22 +0100

    modern-unix-aliases: Check for the packages before aliasing for flexibility

--- files/system_files/base/common/etc/profile.d/modern-unix-aliases.sh
+++ files/system_files/base/common/etc/profile.d/modern-unix-aliases.sh
@@ -1,10 +1,14 @@
 # Aliases for modernized versions of common Linux tools
 
 # bat for cat
-alias cat='bat --theme=ansi --style=plain --pager=never'
+if command -v bat &> /dev/null; then
+    alias cat='bat --theme=ansi --style=plain --pager=never'
+fi
 
 # eza for ls & tree
-alias ls='eza'
-alias ll='eza -lHbg --group-directories-first'
-alias la='eza -lHbgaa --group-directories-first'
-alias tree='eza --tree'
+if command -v eza &> /dev/null; then
+    alias ls='eza'
+    alias ll='eza -lHbg --group-directories-first'
+    alias la='eza -lHbgaa --group-directories-first'
+    alias tree='eza --tree'
+fi
--- files/system_files/base/common/usr/share/fish/vendor_conf.d/modern-unix-aliases.fish
+++ files/system_files/base/common/usr/share/fish/vendor_conf.d/modern-unix-aliases.fish
@@ -1,10 +1,14 @@
 # Aliases for modernized versions of common Linux tools
 
 # bat for cat
-alias cat='bat --theme=ansi --style=plain --pager=never'
+if type -q bat
+    alias cat='bat --theme=ansi --style=plain --pager=never'
+end
 
 # eza for ls & tree
-alias ls='eza'
-alias ll='eza -lHbg --group-directories-first'
-alias la='eza -lHbgaa --group-directories-first'
-alias tree='eza --tree'
+if type -q eza
+    alias ls='eza'
+    alias ll='eza -lHbg --group-directories-first'
+    alias la='eza -lHbgaa --group-directories-first'
+    alias tree='eza --tree'
+end
