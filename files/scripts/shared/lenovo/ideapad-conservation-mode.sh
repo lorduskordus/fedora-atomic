8364c4f8c5c193260942a140d7e92a5827e3e7bb
lorduskordus <lorduskordus@gmail.com>
2026-03-14 20:28:45 +0100

    Allow toggling ideapad conservation mode without root out of the box

---
+++ files/scripts/shared/lenovo/ideapad-conservation-mode.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Allows toggling ideapad conservation mode without root
+
+set -euo pipefail
+
+echo "Allowing to toggle ideapad conservation mode without root"
+
+SUDOERS_LINE="ALL ALL=(ALL) NOPASSWD: /usr/bin/tee /sys/bus/platform/drivers/ideapad_acpi/VPC????:??/conservation_mode"
+SUDOERS_DIR="/etc/sudoers.d"
+SUDOERS_FILENAME="ideapad-conservation_mode"
+SUDOERS_FILE="${SUDOERS_DIR}/${SUDOERS_FILENAME}"
+
+mkdir -p ${SUDOERS_DIR}
+echo ${SUDOERS_LINE} > ${SUDOERS_FILE}
+chmod 0440 ${SUDOERS_FILE}
