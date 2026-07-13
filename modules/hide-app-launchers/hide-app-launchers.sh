c2465dd9150dc7fee7770e1a305fd94678a9fc59
lorduskordus <lorduskordus@gmail.com>
2026-07-13 14:10:22 +0200

    Skip hiding launchers when they're not present

--- modules/hide-app-launchers/hide-app-launchers.sh
+++ modules/hide-app-launchers/hide-app-launchers.sh
@@ -11,22 +11,16 @@ fi
 
 APP_LAUNCHERS_LOCATION="/usr/share/applications"
 
-NF_FLAG=0
 for APP in "${APPS_TO_HIDE[@]}"; do
     if [ ! -f "${APP_LAUNCHERS_LOCATION}/${APP}.desktop" ]; then
-        echo "- App launcher for '${APP}' not found."
-        NF_FLAG=1
+        echo "- App launcher for '${APP}' not found. Skipping."
+        continue
     fi
+
+    APPS_TO_HIDE_FOUND+=("${APP}")
 done
 
-if [ ${NF_FLAG} -eq 1 ]; then
-    echo "Error: Some app launchers could not be found."
-    exit 1
-else
-    echo "All app launchers found."
-fi
-
-for APP in "${APPS_TO_HIDE[@]}"; do
+for APP in "${APPS_TO_HIDE_FOUND[@]}"; do
     if sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' ${APP_LAUNCHERS_LOCATION}/${APP}.desktop; then
         echo "- Successfully hidden '${APP}.desktop'."
     else
@@ -35,4 +29,4 @@ for APP in "${APPS_TO_HIDE[@]}"; do
     fi
 done
 
-echo "All app launchers hidden successfully."
+echo "All found app launchers hidden successfully."
